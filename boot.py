c2e0fa2a813e1682e4894aea2a2ce0935842bb2a
Vincent Kuhlen <vkuhlen@uni-bremen.de>
2019-03-25 08:46:20 +0100

    add wlan scan on boot

--- boot.py
+++ boot.py
@@ -1,11 +1,24 @@
 import network
-import config
+import binascii
+
+from config import Config
 
 _WLAN_SSID = ''
 _WLAN_PASSWORD = ''
 _WLAN_ENCRYPTION = network.WLAN.WPA2
 
 wlan = network.WLAN(mode=network.WLAN.STA)
+
+# Initial WLan scan to get a list of visible SSIDs
+config = Config()
+ssids = [ {
+    'ssid': s.ssid,
+    'bssid': binascii.hexlify(s.bssid).decode('utf-8'),
+    'sec': s.sec,
+    'channel': s.channel} for s in wlan.scan() ]
+config.data['networking']['wlan']['available'] = ssids
+config.write()
+
 wlan.connect(
     _WLAN_SSID,
     auth=(_WLAN_ENCRYPTION, _WLAN_PASSWORD)
